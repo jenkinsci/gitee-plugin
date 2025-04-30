e7ae26d4a89873732662c7051209c8dd40c4c2dc
Andy <137199874+Puffy1215@users.noreply.github.com>
2025-04-30 21:31:28 +0800

    Reworked private class for ResteasyClient (#79)
    - Renamed private class ResteasyClientBuilder to HttpCredsResteasyClientBuilderImpl to avoid duplicate names and better reflect functionality of class
    - Removed getHttpEngine as it holdover from forking from GitLab and their RESTful API version 3

--- src/main/java/com/gitee/jenkins/gitee/api/impl/ResteasyGiteeClientBuilder.java
+++ src/main/java/com/gitee/jenkins/gitee/api/impl/ResteasyGiteeClientBuilder.java
@@ -23,11 +23,8 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.jboss.resteasy.client.jaxrs.ClientHttpEngine;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder.HostnameVerificationPolicy;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient43Engine;
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.jboss.resteasy.client.jaxrs.internal.proxy.ProxyBuilderImpl;
 import org.jboss.resteasy.core.providerfactory.ResteasyProviderFactoryImpl;
@@ -93,7 +90,7 @@ public class ResteasyGiteeClientBuilder extends GiteeClientBuilder {
     }
 
     private GiteeClient buildClient(String url, String apiToken, ProxyConfiguration httpProxyConfig, boolean ignoreCertificateErrors, int connectionTimeout, int readTimeout) {
-        ResteasyClientBuilder builder = new ResteasyClientBuilder();
+        HttpCredsResteasyClientBuilderImpl builder = new HttpCredsResteasyClientBuilderImpl();
 
         if (ignoreCertificateErrors) {
             builder.hostnameVerification(HostnameVerificationPolicy.ANY);
@@ -233,27 +230,18 @@ public class ResteasyGiteeClientBuilder extends GiteeClientBuilder {
         }
     }
 
-    private static class ResteasyClientBuilder extends ResteasyClientBuilderImpl {
+    private static class HttpCredsResteasyClientBuilderImpl extends ResteasyClientBuilderImpl {
         private CredentialsProvider proxyCredentials;
 
+
         @SuppressWarnings("UnusedReturnValue")
-        ResteasyClientBuilder defaultProxy(String hostname, int port, final String scheme, String username, String password) {
+        HttpCredsResteasyClientBuilderImpl defaultProxy(String hostname, int port, final String scheme, String username, String password) {
             super.defaultProxy(hostname, port, scheme);
             if (username != null && password != null) {
                 proxyCredentials = new BasicCredentialsProvider();
                 proxyCredentials.setCredentials(new AuthScope(hostname, port), new UsernamePasswordCredentials(username, password));
             }
             return this;
-        }
-
-        @SuppressWarnings("deprecation")
-        @Override
-        public ClientHttpEngine getHttpEngine() {
-            ApacheHttpClient43Engine httpEngine = (ApacheHttpClient43Engine) super.getHttpEngine();
-            if (proxyCredentials != null) {
-                ((DefaultHttpClient) httpEngine.getHttpClient()).setCredentialsProvider(proxyCredentials);
-            }
-            return httpEngine;
         }
     }
 }
