97a90fb147b9e186797bdb15d58a48b46dadf3c2
Andy Xie <andy.xie.opensource@gmail.com>
2025-05-21 22:38:56 +0800

    Added authentication tests to GiteeConnectionConfigTest.java

--- src/test/java/com/gitee/jenkins/connection/GiteeConnectionConfigTest.java
+++ src/test/java/com/gitee/jenkins/connection/GiteeConnectionConfigTest.java
@@ -12,6 +12,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
@@ -22,18 +24,35 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import com.gitee.jenkins.gitee.api.GiteeClient;
 import com.gitee.jenkins.gitee.api.impl.GiteeV5ClientBuilder;
+import com.gitee.jenkins.trigger.GiteePushTrigger;
+
 import hudson.ProxyConfiguration;
+import hudson.model.FreeStyleProject;
+import hudson.model.Item;
+import hudson.security.GlobalMatrixAuthorizationStrategy;
 import hudson.util.FormValidation;
 import hudson.util.Secret;
 import jakarta.ws.rs.core.Response;
 import jenkins.model.Jenkins;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.jenkinsci.plugins.matrixauth.AuthorizationType;
+import org.jenkinsci.plugins.matrixauth.PermissionEntry;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
@@ -156,5 +175,67 @@ public class GiteeConnectionConfigTest {
         GiteeClient client = config.getClient(connection.getName());
         assertNotNull(client);
         assertSame(client, config.getClient(connection.getName()));
+    }
+
+    @Test
+    void authenticationEnabled_registered_success() throws Exception {
+        String username = "test-user";
+        jenkins.getInstance().setSecurityRealm(jenkins.createDummySecurityRealm());
+        GlobalMatrixAuthorizationStrategy authorizationStrategy = new GlobalMatrixAuthorizationStrategy();
+        authorizationStrategy.add(Item.BUILD, new PermissionEntry(AuthorizationType.EITHER, username));
+        jenkins.getInstance().setAuthorizationStrategy(authorizationStrategy);
+        URL jenkinsURL = jenkins.getURL();
+        jenkins.createFreeStyleProject("test");
+
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(jenkinsURL.toExternalForm() + "project/test");
+        request.addHeader("X-Gitee-Event", "Push Hook");
+        String auth = username + ":" + username;
+        request.addHeader(
+                HttpHeaders.AUTHORIZATION,
+                "Basic " + Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.ISO_8859_1)));
+        request.setEntity(new StringEntity("{}"));
+
+        CloseableHttpResponse response = client.execute(request);
+
+        assertThat(response.getStatusLine().getStatusCode(), is(200));
+    }
+
+    @Test
+    @Disabled // authenticationEnabled_anonymous_forbidden is disabled because of potential bug in codebase (fix coming later)
+    void authenticationEnabled_anonymous_forbidden() throws Exception {
+        boolean defaultValue = jenkins.get(GiteeConnectionConfig.class).isUseAuthenticatedEndpoint();
+        assertTrue(defaultValue);
+        jenkins.getInstance().setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy());
+        URL jenkinsURL = jenkins.getURL();
+        FreeStyleProject project = jenkins.createFreeStyleProject("test");
+        GiteePushTrigger trigger = mock(GiteePushTrigger.class);
+        project.addTrigger(trigger);
+
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(jenkinsURL.toExternalForm() + "project/test");
+        request.addHeader("X-Gitee-Event", "Push Hook");
+        request.setEntity(new StringEntity("{}"));
+
+        CloseableHttpResponse response = client.execute(request);
+
+        assertThat(response.getStatusLine().getStatusCode(), is(403));
+    }
+
+    @Test
+    void authenticationDisabled_anonymous_success() throws Exception {
+        jenkins.get(GiteeConnectionConfig.class).setUseAuthenticatedEndpoint(false);
+        jenkins.getInstance().setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy());
+        URL jenkinsURL = jenkins.getURL();
+        jenkins.createFreeStyleProject("test");
+
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpPost request = new HttpPost(jenkinsURL.toExternalForm() + "project/test");
+        request.addHeader("X-Gitee-Event", "Push Hook");
+        request.setEntity(new StringEntity("{}"));
+
+        CloseableHttpResponse response = client.execute(request);
+
+        assertThat(response.getStatusLine().getStatusCode(), is(200));
     }
 }
