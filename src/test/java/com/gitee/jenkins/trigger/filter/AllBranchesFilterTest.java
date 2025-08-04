02b50166320231bb6554c4092504d18c9277baf6
Andy <137199874+Puffy1215@users.noreply.github.com>
2025-08-04 23:02:02 +0800

    added AllBranchesFilterTest.java nad BranchFilterFactoryTest.java (#114)

---
+++ src/test/java/com/gitee/jenkins/trigger/filter/AllBranchesFilterTest.java
@@ -0,0 +1,20 @@
+package com.gitee.jenkins.trigger.filter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Robin Müller
+ */
+class AllBranchesFilterTest {
+
+    @Test
+    void isRandomBranchNameAllowed() {
+        String randomBranchName = RandomStringUtils.random(10, true, false);
+
+        assertThat(new AllBranchesFilter().isBranchAllowed(randomBranchName), is(true));
+    }
+}
---
+++ src/test/java/com/gitee/jenkins/trigger/filter/BranchFilterFactoryTest.java
@@ -0,0 +1,46 @@
+package com.gitee.jenkins.trigger.filter;
+
+import static com.gitee.jenkins.trigger.filter.BranchFilterConfig.BranchFilterConfigBuilder.branchFilterConfig;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Robin Müller
+ */
+class BranchFilterFactoryTest {
+
+    @Test
+    void getAllBranchesFilter() {
+        BranchFilter branchFilter = BranchFilterFactory.newBranchFilter(branchFilterConfig()
+                .withIncludeBranchesSpec("master")
+                .withExcludeBranchesSpec("develop")
+                .withTargetBranchRegex(".*")
+                .build(BranchFilterType.All));
+
+        assertThat(branchFilter, instanceOf(AllBranchesFilter.class));
+    }
+
+    @Test
+    void getNameBasedFilterFilter() {
+        BranchFilter branchFilter = BranchFilterFactory.newBranchFilter(branchFilterConfig()
+                .withIncludeBranchesSpec("master")
+                .withExcludeBranchesSpec("develop")
+                .withTargetBranchRegex(".*")
+                .build(BranchFilterType.NameBasedFilter));
+
+        assertThat(branchFilter, instanceOf(NameBasedFilter.class));
+    }
+
+    @Test
+    void getRegexBasedFilterFilter() {
+        BranchFilter branchFilter = BranchFilterFactory.newBranchFilter(branchFilterConfig()
+                .withIncludeBranchesSpec("master")
+                .withExcludeBranchesSpec("develop")
+                .withTargetBranchRegex(".*")
+                .build(BranchFilterType.RegexBasedFilter));
+
+        assertThat(branchFilter, instanceOf(RegexBasedFilter.class));
+    }
+}
