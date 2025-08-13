faa86273f8d84777755d559d497daaa9e3d170a2
Andy Xie <andy.xie.opensource@gmail.com>
2025-08-13 21:10:50 +0800

    Added PngAction tests

---
+++ src/test/java/com/gitee/jenkins/webhook/status/BranchStatusPngActionTest.java
@@ -0,0 +1,20 @@
+package com.gitee.jenkins.webhook.status;
+
+import hudson.model.FreeStyleProject;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author Robin Müller
+ */
+@WithJenkins
+@ExtendWith(MockitoExtension.class)
+class BranchStatusPngActionTest extends StatusPngActionTest {
+
+    @Override
+    protected BuildStatusAction getBuildStatusAction(FreeStyleProject project) {
+        return new BranchStatusPngAction(project, branch);
+    }
+}
+
