5c0ae56cbbebe5e18b764a8db0570de99271d73e
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-12-02 18:05:56 +0100

    Generate this secret client-side

--- src/main/resources/com/gitee/jenkins/trigger/GiteePushTrigger/adjunct.js
+++ src/main/resources/com/gitee/jenkins/trigger/GiteePushTrigger/adjunct.js
@@ -1,3 +1,13 @@
-function updateSecretToken(newValue) {
-    document.getElementById('giteeSecretToken').value = newValue;
-}
+Behaviour.specify("BUTTON.gitee-generate", "gitee-generate", 0, function (e) {
+    e.onclick = function (evt) {
+        document.getElementById('giteeSecretToken').value = [...Array(32)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+        evt.preventDefault();
+    };
+});
+
+Behaviour.specify("BUTTON.gitee-clear", "gitee-clear", 0, function (e) {
+    e.onclick = function (evt) {
+        document.getElementById('giteeSecretToken').value = "";
+        evt.preventDefault();
+    };
+});
