94e12c8783d768eca16a000591d6e5eef5b1cf79
Andy <137199874+Puffy1215@users.noreply.github.com>
2025-12-08 23:08:09 +0800

    Remove repeat code in GiteeCreatePullRequestPublisher.java (#153)

--- src/main/java/com/gitee/jenkins/publisher/GiteeCreatePullRequestPublisher.java
+++ src/main/java/com/gitee/jenkins/publisher/GiteeCreatePullRequestPublisher.java
@@ -177,29 +177,6 @@ public class GiteeCreatePullRequestPublisher extends Notifier implements MatrixA
             client.createPullRequest(pr);
             LOGGER.log(Level.INFO, "Pull request {0} generated, {1} -> {2}", LoggerUtil.toArray(title, head, base));
         }
-        
-        if (build.getResult() == Result.SUCCESS) {
-            PullRequest pr = PullRequestBuilder.pullRequest()
-                    .withRepoOwner(owner)
-                    .withRepoPath(repo)
-                    .withTitle(pullRequestTitle)
-                    .withSourceBranch(head)
-                    .withTargetBranch(base)
-                    .withDescription(body)
-                    .build();
-            
-            if (!client.getPullRequest(pr).isEmpty()) {
-                LOGGER.log(Level.INFO, "Pull request {0} -> {1} already exists", LoggerUtil.toArray(head, base));
-                if (launcher != null) {
-                    launcher.getListener().getLogger().println("Pull request {0} -> {1} already exists");
-                }
-                
-                return true;
-            }
-
-            client.createPullRequest(pr);
-            LOGGER.log(Level.INFO, "Pull request {0} generated, {1} -> {2}", LoggerUtil.toArray(title, head, base));
-        }
 
         return true;
     }
