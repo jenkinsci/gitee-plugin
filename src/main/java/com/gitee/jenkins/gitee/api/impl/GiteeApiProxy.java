1a3914c28c90f63a8e269553b24171f396fd492f
Andy <137199874+Puffy1215@users.noreply.github.com>
2025-12-10 23:24:53 +0800

    Add prune source, draft, and squash merge options when creating PR (#154)

--- src/main/java/com/gitee/jenkins/gitee/api/impl/GiteeApiProxy.java
+++ src/main/java/com/gitee/jenkins/gitee/api/impl/GiteeApiProxy.java
@@ -1,18 +1,22 @@
 package com.gitee.jenkins.gitee.api.impl;
 
-
 import java.util.List;
 
 import com.gitee.jenkins.gitee.api.model.*;
 
-
-
 interface GiteeApiProxy {
     void createPullRequestNote(String owner, String repo, Integer pullRequestId, String body);
+
     void headCurrentUser();
+
     void acceptPullRequest(String owner, String repo, Integer pullRequestId);
-    void createPullRequest(String owner, String repo, String title, String base, String head, String body);
+
+    void createPullRequest(String owner, String repo, String title, String base, String head, String body,
+            Boolean pruneSourceBranch, Boolean draft, Boolean squash);
+
     List<PullRequest> getPullRequest(String owner, String repo, String base, String head);
+
     User getCurrentUser();
+
     List<Label> getLabels(String owner, String repo);
 }
